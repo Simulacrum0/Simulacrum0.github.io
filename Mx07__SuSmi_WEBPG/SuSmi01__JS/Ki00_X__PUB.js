9c135f3b7acd13e9df7b86bc5afa30ab0d36e8d7
jkv <Simulacrum0@live.com>
2025-09-03 11:15:05 -0400

    SelfServ Edits v14

--- Mx07__SuSmi_WEBPG/SuSmi01__JS/Ki00_X__PUB.js
+++ Mx07__SuSmi_WEBPG/SuSmi01__JS/Ki00_X__PUB.js
@@ -1,1 +1,1 @@
-const WEBPG__SuGwy_vsg = "https://powerourpeople.com/Mx07__SuSmi_WEBPG/";
+const VER_vsg = "14"; \n //const WEBPG__SuGwy_vsg = "Mx07__SuSmi_WEBPG/"; \n const WEBPG__SuGwy_vsg = "https://powerourpeople.com/Mx07__SuSmi_WEBPG/"; \n
