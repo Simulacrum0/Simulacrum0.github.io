808418093f504d515af6f5ac262cfbeff04260ce
jkv <Simulacrum0@live.com>
2025-09-03 11:22:23 -0400

    SelfServ Edits v17

--- Mx07__SuSmi_WEBPG/SuSmi01__JS/Ki00__PUB.js
+++ Mx07__SuSmi_WEBPG/SuSmi01__JS/Ki00__PUB.js
@@ -1,25 +1,4 @@
-//=============
-// BROWSER
-// SkiLNX
-// $ google-chrome --allow-file-access-from-files --allow-file-access file:///$Ku5_KoKri/Kri07__Mx_POST/index.html
-// SkiMAC
-// $open -a "Google Chrome" --allow-file-access-from-files --allow-file-access file://$HOME/MEGA/Kri07__Mx_POST/index.html
-// SkiWIN
-// $google-chrome --allow-file-access-from-files --allow-file-access file:///mnt/Ku5_KoKri/Kri07__Mx_POST/index.html
+const VER_vsg = "0.17"; 
+ //const WEBPG__SuGwy_vsg = "Mx07__SuSmi_WEBPG/"; 
+ const WEBPG__SuGwy_vsg = "https://powerourpeople.com/Mx07__SuSmi_WEBPG/"; 
 
-const VER_vsg = "0.78";
-
-//=============
-// LAB ( Edit locally using FILES only )
-//=============
-//const WEBPG__SuGwy_vsg = "Mx07__SuSmi_WEBPG/";
-
-//=============
-// PUB ( Publish using REAL Site )
-// $google-chrome powerourpeople.com
-//=============
-const WEBPG__SuGwy_vsg = "https://powerourpeople.com/Mx07__SuSmi_WEBPG/";
-
-//=============
-// END
-//=============
