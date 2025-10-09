b26f4874c7e6992dfa2fa407ef7ef3d0cd9d4203
jkv <Simulacrum0@live.com>
2025-10-09 10:20:45 -0400

    Core Caps Upgrades v9

--- Mx07__SuSmi_WEBPG/SuSmi01__JS/Ki00__PUB.js
+++ Mx07__SuSmi_WEBPG/SuSmi01__JS/Ki00__PUB.js
@@ -1,4 +1,4 @@
-const VER_vsg = "Mx_0.8"; 
+const VER_vsg = "Mx0.9"; 
  //const WEBPG__SuGwy_vsg = "Mx07__SuSmi_WEBPG/"; 
  const WEBPG__SuGwy_vsg = "https://powerourpeople.com/Mx07__SuSmi_WEBPG/"; 
 
