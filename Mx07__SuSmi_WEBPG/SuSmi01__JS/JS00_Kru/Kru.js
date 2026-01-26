3330652fb42e9d48e01821a0aa1eba699ae997b7
jkv <Simulacrum0@live.com>
2026-01-26 17:31:55 -0500

    Core Caps Upgrades v187

--- Mx07__SuSmi_WEBPG/SuSmi01__JS/JS00_Kru/Kru.js
+++ Mx07__SuSmi_WEBPG/SuSmi01__JS/JS00_Kru/Kru.js
@@ -1,5 +1,5 @@
-const BriDzSa__Da_vsg = "PUB_v0.186"; 
- const BriDzSa__Da_wuk = "186"; 
+const BriDzSa__Da_vsg = "PUB_v0.187"; 
+ const BriDzSa__Da_wuk = "187"; 
  const BriDz__Mx_KuTu_vsg = "https://powerourpeople.com/"; 
 
 //==============================================
